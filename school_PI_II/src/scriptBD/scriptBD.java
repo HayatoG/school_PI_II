1e75fe2633681032e66d68bf36c1337b20b27b6f
olive <olive@192.168.1.8>
2020-11-10 02:46:43 -0200

    BD - Script

---
+++ school_PI_II/src/scriptBD/scriptBD.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package scriptBD;
+
+/**
+ *
+ * @author olive
+ */
+public class scriptBD {
+    /*create database if not exists school_pi;
+use school_pi;
+
+create table login  (
+id_login int not null auto_increment,
+login varchar(20),
+senha varchar(20),
+PRIMARY KEY (id_login)
+);
+
+create table estudante (
+id_estudante int not null auto_increment,
+ra varchar(20),
+nome_estudante varchar(40),
+primary key(id_estudante)
+);
+
+create table escola (
+id_escola int not null auto_increment,
+nome_escola varchar(40),
+endereco varchar(40),
+primary key(id_escola)
+);
+
+create table responsavel (
+id_responsavel int not null auto_increment,
+nome_responsavel varchar(40),
+cpf varchar(40),
+primary key(id_responsavel)
+); */
+    
+}
